b8c548a39d0e359a5220e7c046429b22a773c38b
Godwin <godwincyril6@gmail.com>
2025-03-07 19:18:15 +0530

    Initial Commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a target="blank" href="mailto:godwine1100@gmail.com">godwine1100@email.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a target="blank" href="https://www.linkedin.com/in/godwin-e/">https://www.linkedin.com/in/godwin-e/</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a target="blank" href="https://github.com/Godw16">https://github.com/Godw16</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};

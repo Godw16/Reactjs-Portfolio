37ea84d9c7fa47c24e50da30d5ec9d8449d3fe18
Godwin <godwincyril6@gmail.com>
2025-03-13 19:52:15 +0530

    Initial Commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,18 +13,18 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a target="blank" href="mailto:godwine1100@gmail.com">godwine1100@email.com</a>
+          <a target="blank" href="mailto:godwine1100@gmail.com"  rel="noopener noreferrer">godwine1100@email.com</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a target="blank" href="https://www.linkedin.com/in/godwin-e/">https://www.linkedin.com/in/godwin-e/</a>
+          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/godwin-e/">https://www.linkedin.com/in/godwin-e/</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a target="blank" href="https://github.com/Godw16">https://github.com/Godw16</a>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/Godw16">https://github.com/Godw16</a>
         </li>
       </ul>
     </footer>
